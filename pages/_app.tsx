76d1ba0044ac221084bef51e53f3d2b14e1cb04f
Sara Vieira <hey@iamsaravieira.com>
2020-03-27 15:57:38 +0100

    add types for node fetch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NextApp, { Container } from "next/app";
+import NextApp from "next/app";
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
 
 import Navbar from "../components/Navbar";
@@ -8,17 +8,15 @@ class App extends NextApp {
   render() {
     const { Component } = this.props;
     return (
-      <Container>
-        <ThemeProvider>
-          <CSSReset />
-          <ColorModeProvider>
-            <>
-              <Navbar {...this.props.pageProps} />
-              <Component {...this.props.pageProps} />
-            </>
-          </ColorModeProvider>
-        </ThemeProvider>
-      </Container>
+      <ThemeProvider>
+        <CSSReset />
+        <ColorModeProvider>
+          <>
+            <Navbar {...this.props.pageProps} />
+            <Component {...this.props.pageProps} />
+          </>
+        </ColorModeProvider>
+      </ThemeProvider>
     );
   }
 }
